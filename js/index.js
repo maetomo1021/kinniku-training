29b22df8fe102bb979dce0bcfb1fd86a4499dbed
maetomo1021 <maetomoda@gmail.com>
2023-11-12 17:33:45 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-var test = "";
-var syozaiti = document.getElementById("itizyouhou");
-var kizaizyouhou = document.getElementById("tanmatu");
-// Syozaiti.innerHTML ="dadada";
-
 
 /* 日にち、曜日の取得 */
 window.addEventListener("DOMContentLoaded", function () {
@@ -18,11 +13,11 @@ window.addEventListener("DOMContentLoaded", function () {
     var Hiniti = document.getElementById("date").innerHTML = ("今日の日付は：" + year + "年" + month + "月" + date + "日" + "(" + weekday[day] + ")")
     // console.log(Hiniti)
     const Nengappi = (year + "年" + month + "月" + date + "日" + "(" + weekday[day] + ")")
+
     //Canvasのオブジェクトの作成
     // var canvas = document.getElementById("canvas-area")
     // var ctx= canvas.getContext('2d');     
 });
-
 
 /* 位置情報の取得 */
 window.addEventListener("DOMContentLoaded", function () {
@@ -30,7 +25,7 @@ window.addEventListener("DOMContentLoaded", function () {
     // var position =  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
     if (navigator.geolocation) {
         // Geolocation APIをサポートしている場合の処理
-        console.log("hello")
+        console.log("以下、位置情報関係の処理してます")
         const watchId = navigator.geolocation.watchPosition(success, error);
 
         function success(position) {
@@ -38,8 +33,8 @@ window.addEventListener("DOMContentLoaded", function () {
             const longitude = position.coords.longitude;
             console.log(latitude)
             console.log(longitude)
-            a = ("緯度：" + longitude + "軽度" + latitude)
-            // 取得した位置情報を活用する処理
+            a = ("緯度：" + longitude + "  " + "経度" + latitude)
+            document.querySelector("#syozaiti").textContent = a
         }
 
         function error(error) {
@@ -51,12 +46,9 @@ window.addEventListener("DOMContentLoaded", function () {
         console.log("No")
     }
 
-
-});
-
-
-/* 端末情報の取得 */
-window.addEventListener("DOMContentLoaded", function () {
+    /* 端末情報の取得 */
+    var syozaiti = document.getElementById("itizyouhou");
+    var kizaizyouhou = document.getElementById("tanmatu");
     const browserName = navigator.appName;
     const browserVersion = navigator.appVersion;
     const osName = navigator.platform;
@@ -66,7 +58,16 @@ window.addEventListener("DOMContentLoaded", function () {
     console.log("OS名: " + osName);
     // location.href = "https://www.google.co.jp/maps/place/35%C2%B041'29.5%22N+139%C2%B041'48.3%22E/@35.6915291,139.6941813"
     console.log("111111111111111111111111")
-    console.log(syozaiti)
+    // console.log(syozaiti)
     console.log(kizaizyouhou)
 
-})
+});
+
+//東京の天気情報を取得する
+
+
+
+
+
+
+
